d7c51b673a79eccd42d6a029f04fd5f1446ebd79
jon-vdb <jonvdberg8@gmail.com>
2023-07-01 14:23:36 +0700

    removed ./container inport

--- app/help/page.tsx
+++ app/help/page.tsx
@@ -2,13 +2,8 @@
 import React from "react";
 import Link from "next/link";
 import Button from "../../components/ui/button";
-import Help from "@/components/ui/help";
 import { ArrowLeftSquare } from "lucide-react";
 
 export default function page() {
-  return (
-    <div className="">
-      <Help />
-    </div>
-  );
+  return <div className="">HELP</div>;
 }
