1bb682a32befdf6730db2d040fdec54cc78d5de0
jon-vdb <jonvdberg8@gmail.com>
2023-07-01 14:05:17 +0700

    deploy fixes

--- app/identification/page.tsx
+++ app/identification/page.tsx
@@ -1,16 +1,11 @@
 "use client";
 
 import QrScanner from "@/components/ui/QrScanner";
-import Container from "@/components/ui/container";
-import MediaDevices from "@/components/ui/media-devices";
 import Result from "@zxing/library/esm/core/Result";
 
-import { SetStateAction, useState } from "react";
-import GenerateQRCode from "../utilities/generateQRCode";
-import CameraComponent from "@/components/ui/profile-selfie";
-import { captureAndSendImage } from "../../api/profile-image";
-import { Skeleton } from "@/components/ui/skeleton";
 import IdentificationInfo from "@/components/ui/identification-info.";
+import { SetStateAction, useState } from "react";
+import { captureAndSendImage } from "../../api/profile-image";
 
 export default function Page() {
   const initData = {
--- components/ui/Container.tsx
+++ components/ui/Container.tsx
@@ -1,5 +1,7 @@
 import React, { ReactElement } from "react";
 
-export default function Container({ children }: { children: ReactElement }) {
+function Container({ children }: { children: ReactElement }) {
   return <div className="px-3 py-6">{children}</div>;
 }
+
+export default Container;
